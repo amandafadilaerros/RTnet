6c926da19a734430fc2d6c08030c599d5dbd7f81
amandafadilaerros <amandafadila1@gmail.com>
2024-04-03 12:44:30 +0700

    tambah template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Login
+Route::get('/login', function () {
+    return view('login');
 });
