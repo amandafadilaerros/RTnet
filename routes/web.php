76e229c5daa10229bf5c8ae5d67f12d654de3bbe
Yoga Pradana <yogapradana5666@gmail.com>
2024-04-22 13:09:28 +0700

    Penambahan route inventaris penduduk

--- routes/web.php
+++ routes/web.php
@@ -90,6 +90,22 @@ Route::group(['prefix' => 'bendahara'], function () {
     Route::get('/akunBendahara', [bendaharaController::class, 'akun']);
 });
 
-Route::get('/DaftarAnggota', [DaftarAnggotaController::class, 'index']);
-Route::get('/Dashboard', [DashboardController::class, 'index']);
-Route::get('/inventaris',[inventarisController::class, 'index']);
+Route::group(['prefix' => 'penduduk'], function () {
+    Route::get('/dashboard', [pendudukController::class, 'index']);
+    Route::get('/DaftarAnggota', [DaftarAnggotaController::class, 'index']);
+    Route::get('/laporan_keuangan', [pendudukController::class, 'keuangan']);
+    Route::get('/peminjaman', [peminjamanController::class, 'index']);
+    Route::get('/keuangan', [pendudukController::class, 'keuangan']);
+    Route::get('/kerja_bakti', [pendudukController::class, 'kegiatan']);
+    Route::get('/pengumuman', [pendudukController::class, 'pengumuman']);
+    Route::get('/akun', [pendudukController::class, 'akun']);
+    Route::get('/inventaris_penduduk',[inventarisController::class, 'pk']);
+Route::get('/inventaris_penduduk_peminjaman',[inventarisController::class, 'pk_peminjaman']);
+
+});
+
+//halaman tidak ditemukan
+Route::fallback(function () {
+    return view('404');
+});
+
