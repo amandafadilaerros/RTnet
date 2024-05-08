6cd70bc159623501b0219fdad1c1e609bd7e1cf5
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-08 12:36:13 +0700

    peminjaman ketua rt

--- app/Models/peminjaman_inventaris.php
+++ app/Models/peminjaman_inventaris.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class peminjaman_inventaris extends Model
 {
     protected $table = 'peminjaman_inventaris';
+    protected $primaryKey = 'id_peminjaman';
+    protected $fiilable = ['id_peminjaman', 'id_inventaris', 'id_peminjam', 'jumlah_peminjaman', 'tanggal_peminjaman', 'tanggal_kembali'];
 
     // Definisikan relasi belongsTo ke model Inventaris
     public function inventaris()
