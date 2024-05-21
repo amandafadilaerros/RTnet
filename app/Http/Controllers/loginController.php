58d978e35dc7c16df02d2e27ba28094cb1cdb0ed
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-21 20:00:47 +0700

    peminjaman ketua penambahan migration spk

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -121,6 +121,7 @@ class loginController extends Controller
         $activeMenu = 'dashboard';
 
         // Switch based on the session role
+        // dd();
         switch ($sessionRole) {
             case 'ketua_rt':
                 return view('ketuaRT.dashboardKetuaRt', [
