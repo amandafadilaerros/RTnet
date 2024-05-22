ae94da1763d04657932f406650b47fc216a74c5f
Yoga Pradana <yogapradana5666@gmail.com>
2024-05-22 13:44:58 +0700

    penyeseuaiane error

--- app/Models/ktp.php
+++ app/Models/ktp.php
@@ -11,8 +11,6 @@ class ktp extends Model
     protected $table = 'ktps'; //mendefiniskan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'NIK'; //mendefiniskan primary key dari tabel yang digunakan
 
-    protected $fillable = ['no_kk','nama', 'tempat', 'tanggal_lahir', 'jenis_kelamin', 'golongan_darah', 'agama', 'status_perkawinan', 'pekerjaan', 'status_keluarga', 'status_anggota', 'jenis_penduduk', 'dokumen'];
-
     public function peminjaman_inventaris(){
         return $this->hasMany(peminjaman_inventaris::class, 'id_peminjam', 'NIK');
     }
