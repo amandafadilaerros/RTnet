2a6b29963d3f94cc6a8d5df22e2869a75ed39de2
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-26 16:08:58 +0700

    test maut

--- app/Http/Controllers/mautController.php
+++ app/Http/Controllers/mautController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Alternatif;
-use App\Models\Matrik;
+use App\Models\alternatif;
+use App\Models\matrik;
 use App\Models\Kriteria;
 
 class mautController extends Controller
