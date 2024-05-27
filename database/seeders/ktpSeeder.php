e937929ed4636dfb18ea948144ef3ce62bb1c869
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-27 10:05:20 +0700

    ktpseeder

--- database/seeders/ktpSeeder.php
+++ database/seeders/ktpSeeder.php
@@ -14,7 +14,7 @@ class ktpSeeder extends Seeder
     public function run(): void
     {
         $data =[
-            ['NIK'=>1212,'no_kk'=>12345678,'nama'=>'Yoga', 'tempat'=>'Malang', 'tanggal_lahir'=>'2024-05-21', 'jenis_kelamin'=>'l', 'golongan_darah'=>'A', 'agama'=>'Islam', 'status_perkawinan'=>'belum kawin','pekerjaan'=>'mahasiswa', 'status_keluarga'=>'Anak', 'status_anggota'=>'keluarga', 'jenis_penduduk'=>'Tetap'],
+            ['NIK'=>1213,'no_kk'=>12345678,'nama'=>'Yoga', 'tempat'=>'Malang', 'tanggal_lahir'=>'2024-05-21', 'jenis_kelamin'=>'l', 'golongan_darah'=>'A', 'agama'=>'Islam', 'status_perkawinan'=>'belum kawin','pekerjaan'=>'mahasiswa', 'status_keluarga'=>'Anak', 'status_anggota'=>'keluarga', 'jenis_penduduk'=>'Tetap'],
             ['NIK'=>2121,'no_kk'=>32345678,'nama'=>'Rafli', 'tempat'=>'Malang', 'tanggal_lahir'=>'2024-05-21', 'jenis_kelamin'=>'l', 'golongan_darah'=>'A', 'agama'=>'Islam', 'status_perkawinan'=>'belum kawin','pekerjaan'=>'mahasiswa', 'status_keluarga'=>'Anak', 'status_anggota'=>'keluarga', 'jenis_penduduk'=>'Tetap'],
             ['NIK'=>3212,'no_kk'=>22345678,'nama'=>'Doni', 'tempat'=>'Malang', 'tanggal_lahir'=>'2024-05-21', 'jenis_kelamin'=>'l', 'golongan_darah'=>'A', 'agama'=>'Islam', 'status_perkawinan'=>'belum kawin','pekerjaan'=>'mahasiswa', 'status_keluarga'=>'Anak', 'status_anggota'=>'keluarga', 'jenis_penduduk'=>'Tetap'],
             ['NIK'=>4213,'no_kk'=>42345678,'nama'=>'Haris', 'tempat'=>'Malang', 'tanggal_lahir'=>'2024-05-21', 'jenis_kelamin'=>'l', 'golongan_darah'=>'A', 'agama'=>'Islam', 'status_perkawinan'=>'belum kawin','pekerjaan'=>'mahasiswa', 'status_keluarga'=>'Anak', 'status_anggota'=>'keluarga', 'jenis_penduduk'=>'Tetap'],
