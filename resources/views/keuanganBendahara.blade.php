773d4f7cfa735e0d4e1bc973edc1efd42da04361
Yunila Putmasari <nilaputma26@gmail.com>
2024-05-28 10:08:16 +0700

    perbaikan bendahara filter dan perbaikan daftar inventaris

--- resources/views/keuanganBendahara.blade.php
+++ resources/views/keuanganBendahara.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 <div class="row">
-  <div class="col-md-6">
-  </div>
+    <div class="col-md-3 mb-4">
+        <div class="input-group float-right">
+            <select class="form-control" id="searchOption" style="border-radius: 20px;">
+                <option value="" selected disabled>Filter</option>
+                <option value="kas">Kas</option>
+                <option value="paguyuban">Paguyuban</option>
+            </select>
+        </div>
+      </div>
   <div class="col-md-6">
     <div class="row justify-content-end">
       <form id="searchForm" class="form-inline">
@@ -41,6 +48,11 @@
   .dataTables_filter {
       display: none;
   }
+  #searchOption option:hover {
+    outline: none; /* Remove default focus outline */
+    border-color: #424874; /* Change border color to match the desired color */
+    box-shadow: 0 0 0 0.2rem rgba(66, 72, 116, 0.25); /* Add a box shadow to simulate focus effect */
+  }
 </style>
 
     <!-- Tambahkan CSS tambahan jika diperlukan -->
@@ -49,56 +61,58 @@
 <script>
     $(document).ready(function() {
         var dataLaporan = $('#laporan').DataTable({
-            serverSide: true, //jika ingin menggunakan server side processing
+            serverSide: true,
             ajax: {
                 "url": "{{ url('bendahara/laporan/list') }}",
                 "dataType": "json",
                 "type": "POST",
                 "data": function(d) {
                     d.search = $('#search').val();
+                    d.filter = $('#searchOption').val(); // Tambahkan filter ke data yang dikirim
                 }
             },
             columns: [{
-                data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColimn()
+                data: "DT_RowIndex",
                 className: "text-center",
                 orderable: false,
                 searchable: false
             }, {
                 data: "jenis_iuran",
                 className: "",
-                orderable: true, //jika ingin kolom bisa urut
-                searchable: true // jika kolom bisa dicari
+                orderable: true,
+                searchable: true
             }, {
                 data: "keterangan",
                 className: "",
-                orderable: true, //jika ingin kolom bisa urut
-                searchable: true // jika kolom bisa dicari
-            },{
+                orderable: true,
+                searchable: true
+            }, {
                 data: "jumlah_uang_masuk",
                 className: "",
-                orderable: true, //jika ingin kolom bisa urut
-                searchable: true // jika kolom bisa dicari
+                orderable: true,
+                searchable: true
             }, {
                 data: "jumlah_uang_keluar",
                 className: "",
-                orderable: true, //jika ingin kolom bisa urut
-                searchable: true // jika kolom bisa dicari
+                orderable: true,
+                searchable: true
             }, {
                 data: "saldo",
                 className: "",
-                orderable: true, //jika ingin kolom bisa urut
-                searchable: true // jika kolom bisa dicari
+                orderable: true,
+                searchable: true
             }]
         });
-        $('#id_iuran').on('change', function() {
+
+        $('#searchForm').on('submit', function(e) {
+            e.preventDefault();
             dataLaporan.ajax.reload();
         });
 
-
-        $('#searchForm').on('submit', function(e) {
-            e.preventDefault(); // Mencegah form untuk submit
-            dataLaporan.ajax.reload(); // Memuat ulang data tabel dengan pencarian yang baru
+        $('#searchOption').on('change', function() {
+            dataLaporan.ajax.reload(); // Memuat ulang data tabel ketika filter diubah
         });
     });
 </script>
+
 @endpush
