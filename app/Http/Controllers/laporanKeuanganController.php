524eec7bcb12aebe52fe1d35363d123c7e773708
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-29 11:16:33 +0700

    test controller

--- app/Http/Controllers/laporanKeuanganController.php
+++ app/Http/Controllers/laporanKeuanganController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\IuranModel;
+use App\Models\iuranModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use JeroenNoten\LaravelAdminLte\View\Components\Tool\Datatable;
