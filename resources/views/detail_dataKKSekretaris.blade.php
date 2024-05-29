5c48637c8a3bebd5d7cf71eadcfd477ad178f31f
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-05-29 12:53:06 +0700

    perbaikan tampilan kk, ktp, rumah

--- resources/views/detail_dataKKSekretaris.blade.php
+++ resources/views/detail_dataKKSekretaris.blade.php
@@ -629,6 +629,7 @@
         $(document).ready(function() {
             var detailKK = $('#table_detail_data_anggota_kk').DataTable({
                 serverSide: true,   //jika ingin menggunakan server side processing
+                searching: false,
                 ajax: {
                     "url": "{{ url('sekretaris/detail_kk/list') }}",
                     "dataType": "json",
@@ -750,6 +751,7 @@
         $(document).ready(function() {
             var detailKK = $('#table_detail_data_non_anggota_kk').DataTable({
                 serverSide: true,   //jika ingin menggunakan server side processing
+                searching: false,
                 ajax: {
                     "url": "{{ url('sekretaris/detail_kk/list2') }}",
                     "dataType": "json",
