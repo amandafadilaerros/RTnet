3ff5d0130884fcf4c9e83d756c54b2e2af6b33ba
Yayun Eldina <yayuneldina02@gmail.com>
2024-06-05 00:29:15 +0700

    Pembaruan sekretaris

--- app/Http/Controllers/data_kkSekretarisController.php
+++ app/Http/Controllers/data_kkSekretarisController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\akun;
 use Illuminate\Http\Request;
 use App\Models\kkModel;
+use App\Models\rumahModel;
 use App\Models\ktp;
 use App\Models\ktpModel;
 use App\Models\level;
@@ -24,9 +25,11 @@ class data_kkSekretarisController extends Controller
         ];
         $activeMenu = 'data_kk';
 
+        $rumah = rumahModel::all(); //mengambil semua data rumah dari modal
         return view('data_KKrt', [
             'breadcrumb' => $breadcrumb,
             'page' => $page,
+            'rumah' => $rumah,
             'activeMenu' => $activeMenu,
         ]);
     }
@@ -42,9 +45,11 @@ class data_kkSekretarisController extends Controller
         ];
         $activeMenu = 'data_kk';
 
+        $rumah = rumahModel::all(); //mengambil semua data rumah dari modal
         return view('data_KKSekretaris', [
             'breadcrumb' => $breadcrumb,
             'page' => $page,
+            'rumah' => $rumah,
             'activeMenu' => $activeMenu,
         ]);
     }
