c876668119bdc74031b0c1aa22d41e9592376b00
Ravsanjani Rafli Arafat <rafliarafat12@gmail.com>
2024-06-04 10:58:07 +0700

    perbaikan otentikasi

--- routes/web.php
+++ routes/web.php
@@ -51,6 +51,7 @@ Route::get('/', function () {
     return view('login');
 
 });
+Route::get('/logout', [loginController::class, 'logout']);
 
 Route::post('/dashboard', [loginController::class, 'login']);
 
